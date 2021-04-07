2d768e17c46a7e70281fef0409cff8e0fe770d7d
rmoser3 <76820286+rmoser3@users.noreply.github.com>
2021-04-07 16:32:43 -0400

    Update UserControl1.xaml.cs

--- GUI/Senior Project/UserControl1.xaml.cs
+++ GUI/Senior Project/UserControl1.xaml.cs
@@ -32,6 +32,8 @@ namespace Senior_Project
         PerspectiveCamera myPCamera = new PerspectiveCamera();
         Model3DGroup model = new Model3DGroup();
         Vector3D rotVector = new Vector3D(0, 0, -1);
+        double xMaxi = 300;
+        double yMaxi = 300;
         double scaleX = 1;
         double scaleY = 1;
         double scaleZ = 1;
@@ -78,7 +80,7 @@ namespace Senior_Project
             viewPort3d.KeyDown += new KeyEventHandler(setRotationAxis);
             viewPort3d.KeyDown += new KeyEventHandler(moveUpDown);
             viewPort3d.KeyDown += new KeyEventHandler(downToTable);
-
+            viewPort3d.KeyDown += new KeyEventHandler(setRotateHandle);
             // Defines the camera used to view the 3D object. In order to view the 3D object,
             // the camera must be positioned and pointed such that the object is within view
             // of the camera.
@@ -318,8 +320,8 @@ namespace Senior_Project
             zLine.Color = Colors.Black;
 
             Point3D center = new Point3D(0, 0, 0);
-            Point3D yPoint = new Point3D(0, 200, 0);
-            Point3D xPoint = new Point3D(200, 0, 0);
+            Point3D yPoint = new Point3D(0, yMaxi, 0);
+            Point3D xPoint = new Point3D(xMaxi, 0, 0);
             Point3D zPoint = new Point3D(0, 0, 200);
 
             yLine.Points.Add(center);
@@ -334,166 +336,29 @@ namespace Senior_Project
             viewPort3d.Children.Add(zLine);
 
             //Grid lines
-            LinesVisual3D xLine2 = new LinesVisual3D();
-            Point3D xPoint2 = new Point3D(20, 0, 0);
-            Point3D xPoint3 = new Point3D(20, 200, 0);
-            xLine2.Points.Add(xPoint2);
-            xLine2.Points.Add(xPoint3);
-            xLine2.Color = Colors.Black;
-            viewPort3d.Children.Add(xLine2);
+            for (int i = 20; i <= xMaxi; i += 20)
+            {
+                LinesVisual3D xLine1 = new LinesVisual3D();
+                LinesVisual3D yLine1 = new LinesVisual3D();
 
-            LinesVisual3D xLine3 = new LinesVisual3D();
-            Point3D xPoint4 = new Point3D(40, 0, 0);
-            Point3D xPoint5 = new Point3D(40, 200, 0);
-            xLine3.Points.Add(xPoint4);
-            xLine3.Points.Add(xPoint5);
-            xLine3.Color = Colors.Black;
-            viewPort3d.Children.Add(xLine3);
+                Point3D xPoint30 = new Point3D(i, 0, 0);
+                Point3D xPoint31 = new Point3D(i, xMaxi, 0);
+                xLine1.Points.Add(xPoint30);
+                xLine1.Points.Add(xPoint31);
+                xLine1.Color = Colors.Black;
 
-            LinesVisual3D xLine4 = new LinesVisual3D();
-            Point3D xPoint6 = new Point3D(60, 0, 0);
-            Point3D xPoint7 = new Point3D(60, 200, 0);
-            xLine4.Points.Add(xPoint6);
-            xLine4.Points.Add(xPoint7);
-            xLine4.Color = Colors.Black;
-            viewPort3d.Children.Add(xLine4);
+                Point3D yPoint30 = new Point3D(0, i, 0);
+                Point3D yPoint31 = new Point3D(xMaxi, i, 0);
+                yLine1.Points.Add(yPoint30);
+                yLine1.Points.Add(yPoint31);
+                yLine1.Color = Colors.Black;
 
-            LinesVisual3D xLine5 = new LinesVisual3D();
-            Point3D xPoint8 = new Point3D(80, 0, 0);
-            Point3D xPoint9 = new Point3D(80, 200, 0);
-            xLine5.Points.Add(xPoint8);
-            xLine5.Points.Add(xPoint9);
-            xLine5.Color = Colors.Black;
-            viewPort3d.Children.Add(xLine5);
+                viewPort3d.Children.Add(xLine1);
+                viewPort3d.Children.Add(yLine1);
 
-            LinesVisual3D xLine6 = new LinesVisual3D();
-            Point3D xPoint10 = new Point3D(100, 0, 0);
-            Point3D xPoint11 = new Point3D(100, 200, 0);
-            xLine6.Points.Add(xPoint10);
-            xLine6.Points.Add(xPoint11);
-            xLine6.Color = Colors.Black;
-            viewPort3d.Children.Add(xLine6);
-
-            LinesVisual3D xLine7 = new LinesVisual3D();
-            Point3D xPoint12 = new Point3D(120, 0, 0);
-            Point3D xPoint13 = new Point3D(120, 200, 0);
-            xLine7.Points.Add(xPoint12);
-            xLine7.Points.Add(xPoint13);
-            xLine7.Color = Colors.Black;
-            viewPort3d.Children.Add(xLine7);
-
-            LinesVisual3D xLine8 = new LinesVisual3D();
-            Point3D xPoint14 = new Point3D(140, 0, 0);
-            Point3D xPoint15 = new Point3D(140, 200, 0);
-            xLine8.Points.Add(xPoint14);
-            xLine8.Points.Add(xPoint15);
-            xLine8.Color = Colors.Black;
-            viewPort3d.Children.Add(xLine8);
-
-            LinesVisual3D xLine9 = new LinesVisual3D();
-            Point3D xPoint16 = new Point3D(160, 0, 0);
-            Point3D xPoint17 = new Point3D(160, 200, 0);
-            xLine9.Points.Add(xPoint16);
-            xLine9.Points.Add(xPoint17);
-            xLine9.Color = Colors.Black;
-            viewPort3d.Children.Add(xLine9);
-
-            LinesVisual3D xLine10 = new LinesVisual3D();
-            Point3D xPoint18 = new Point3D(180, 0, 0);
-            Point3D xPoint19 = new Point3D(180, 200, 0);
-            xLine10.Points.Add(xPoint18);
-            xLine10.Points.Add(xPoint19);
-            xLine10.Color = Colors.Black;
-            viewPort3d.Children.Add(xLine10);
-
-            LinesVisual3D xLine11 = new LinesVisual3D();
-            Point3D xPoint20 = new Point3D(200, 0, 0);
-            Point3D xPoint21 = new Point3D(200, 200, 0);
-            xLine11.Points.Add(xPoint20);
-            xLine11.Points.Add(xPoint21);
-            xLine11.Color = Colors.Black;
-            viewPort3d.Children.Add(xLine11);
-
-            LinesVisual3D yLine2 = new LinesVisual3D();
-            Point3D yPoint2 = new Point3D(0, 20, 0);
-            Point3D yPoint3 = new Point3D(200, 20, 0);
-            yLine2.Points.Add(yPoint2);
-            yLine2.Points.Add(yPoint3);
-            yLine2.Color = Colors.Black;
-            viewPort3d.Children.Add(yLine2);
-
-            LinesVisual3D yLine3 = new LinesVisual3D();
-            Point3D yPoint4 = new Point3D(0, 40, 0);
-            Point3D yPoint5 = new Point3D(200, 40, 0);
-            yLine3.Points.Add(yPoint4);
-            yLine3.Points.Add(yPoint5);
-            yLine3.Color = Colors.Black;
-            viewPort3d.Children.Add(yLine3);
-
-            LinesVisual3D yLine4 = new LinesVisual3D();
-            Point3D yPoint6 = new Point3D(0, 60, 0);
-            Point3D yPoint7 = new Point3D(200, 60, 0);
-            yLine4.Points.Add(yPoint6);
-            yLine4.Points.Add(yPoint7);
-            yLine4.Color = Colors.Black;
-            viewPort3d.Children.Add(yLine4);
-
-            LinesVisual3D yLine5 = new LinesVisual3D();
-            Point3D yPoint8 = new Point3D(0, 80, 0);
-            Point3D yPoint9 = new Point3D(200, 80, 0);
-            yLine5.Points.Add(yPoint8);
-            yLine5.Points.Add(yPoint9);
-            yLine5.Color = Colors.Black;
-            viewPort3d.Children.Add(yLine5);
-
-            LinesVisual3D yLine6 = new LinesVisual3D();
-            Point3D yPoint10 = new Point3D(0, 100, 0);
-            Point3D yPoint11 = new Point3D(200, 100, 0);
-            yLine6.Points.Add(yPoint10);
-            yLine6.Points.Add(yPoint11);
-            yLine6.Color = Colors.Black;
-            viewPort3d.Children.Add(yLine6);
-
-            LinesVisual3D yLine7 = new LinesVisual3D();
-            Point3D yPoint12 = new Point3D(0, 120, 0);
-            Point3D yPoint13 = new Point3D(200, 120, 0);
-            yLine7.Points.Add(yPoint12);
-            yLine7.Points.Add(yPoint13);
-            yLine7.Color = Colors.Black;
-            viewPort3d.Children.Add(yLine7);
-
-            LinesVisual3D yLine8 = new LinesVisual3D();
-            Point3D yPoint14 = new Point3D(0, 140, 0);
-            Point3D yPoint15 = new Point3D(200, 140, 0);
-            yLine8.Points.Add(yPoint14);
-            yLine8.Points.Add(yPoint15);
-            yLine8.Color = Colors.Black;
-            viewPort3d.Children.Add(yLine8);
-
-            LinesVisual3D yLine9 = new LinesVisual3D();
-            Point3D yPoint16 = new Point3D(0, 160, 0);
-            Point3D yPoint17 = new Point3D(200, 160, 0);
-            yLine9.Points.Add(yPoint16);
-            yLine9.Points.Add(yPoint17);
-            yLine9.Color = Colors.Black;
-            viewPort3d.Children.Add(yLine9);
-
-            LinesVisual3D yLine10 = new LinesVisual3D();
-            Point3D yPoint18 = new Point3D(0, 180, 0);
-            Point3D yPoint19 = new Point3D(200, 180, 0);
-            yLine10.Points.Add(yPoint18);
-            yLine10.Points.Add(yPoint19);
-            yLine10.Color = Colors.Black;
-            viewPort3d.Children.Add(yLine10);
-
-            LinesVisual3D yLine11 = new LinesVisual3D();
-            Point3D yPoint20 = new Point3D(0, 200, 0);
-            Point3D yPoint21 = new Point3D(200, 200, 0);
-            yLine11.Points.Add(yPoint20);
-            yLine11.Points.Add(yPoint21);
-            yLine11.Color = Colors.Black;
-            viewPort3d.Children.Add(yLine11);
+            }
         }
+
 
         private Point3D getCenter()
         {
@@ -577,7 +442,7 @@ namespace Senior_Project
                     zMax = points[i].Z;
                 }
             }
-            if ((xMin >= 0) && (xMax <= 200) && (yMin >= 0) && (yMax <= 200) && (zMin >= 0) && (zMax <= 200))
+            if ((xMin >= 0) && (xMax <= xMaxi) && (yMin >= 0) && (yMax <= yMaxi) && (zMin >= 0) && (zMax <= 200))
             {
                 return true;
             }
@@ -600,7 +465,7 @@ namespace Senior_Project
 
         private void set()
         {
-            startPos = new Point3D(100, 100, zLow);
+            startPos = new Point3D(xMaxi/2, yMaxi/2, zLow);
             TranslateTransform3D trans = new TranslateTransform3D(startPos.X - currentPosition.X, startPos.Y - currentPosition.Y, startPos.Z);
             obj.Transform = trans;
             currentPosition = startPos;
@@ -610,7 +475,7 @@ namespace Senior_Project
         {
             if (e.Key == Key.B)
             {
-                startPos = new Point3D(100, 100, zLow);
+                startPos = new Point3D(xMaxi/2, yMaxi/2, zLow);
                 TranslateTransform3D trans = new TranslateTransform3D(startPos.X - centerPoint.X, startPos.Y - centerPoint.Y, zLow);
                 obj.Transform = trans;
                 currentPosition = startPos;
@@ -693,5 +558,26 @@ namespace Senior_Project
                 currentPosition.Z = zLow;
             }
         }
+
+        private void setRotateHandle(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.P)
+            {
+                setRotate();
+            }
+        }
+
+        private void setRotate()
+        {
+            degree = 90;
+            Transform3DGroup trans3Dgroup = new Transform3DGroup();
+            TranslateTransform3D trans = new TranslateTransform3D(startPos.X - centerPoint.X, startPos.Y - centerPoint.Y, zLow);
+            RotateTransform3D rot = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(1,0,0), degree), currentPosition);
+            trans3Dgroup.Children.Add(trans);
+            trans3Dgroup.Children.Add(rot);
+            obj.Transform = trans3Dgroup;
+
+        }
     }
 }
+
