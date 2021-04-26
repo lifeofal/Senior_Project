57b80ae27e29e7a4d97483792518ebda58f03f2b
Tristan Peters <dogburtjunior@gmail.com>
2021-04-26 18:06:42 -0400

    Revert "Added perimeter, layer, and toolpath generation"
    This reverts commit 0c60b67140eeb5e3881263a1decb930ab6170a47.

--- Slicing/test/tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-Generator g1("../settings/config.ini");
-
-    g1.open_File();
-    while(!m.empty())
-    {
-        g1.print_XYE(m.front());
-        m.pop();
-    }
---
+++ Toolpath_gen/test/sandbox.cpp
@@ -0,0 +1,22 @@
+#include "../lib/toolpath_settings.h"
+#include "../lib/toolpath_settings.cpp"
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iterator>
+#include <map>
+
+using namespace std;
+
+
+
+int main()
+{
+    Settings setting("../settings/config.ini");
+
+    float setting1 = setting.f_get_setting("wipe");
+    cout<<setting1;
+    string setting2 = setting.s_get_setting("end_filament_gcode");
+    cout<<setting2;
+}
